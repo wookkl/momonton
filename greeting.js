3ba90890b33b0fec50fff3650654decedecf46aa
wookkl <57362359+wookkl@users.noreply.github.com>
2020-09-01 01:13:00 +0900

    Finish CSS

--- greeting.js
+++ greeting.js
@@ -1,8 +1,11 @@
 const form = document.querySelector(".js-form"),
   input = form.querySelector("input"),
-  greeting = document.querySelector(".js-greetings");
+  greeting = document.querySelector(".js-greetings"),
+  clock = document.querySelector(".js-clock"),
+  toDo = document.querySelector(".js-toDoForm");
 const USER_LS = "currentUser",
   SHOWING_ON = "showing";
+SHOWING_ON_FLEX = "showing-flex";
 function saveName(text) {
   localStorage.setItem(USER_LS, text);
 }
@@ -13,14 +16,16 @@ function handleSubmit(event) {
   saveName(currentValue);
 }
 function askForName() {
-  form.classList.add(SHOWING_ON);
+  form.classList.add(SHOWING_ON_FLEX);
   form.addEventListener("submit", handleSubmit);
 }
 
 function paintGreeting(text) {
-  form.classList.remove(SHOWING_ON);
+  toDo.classList.add(SHOWING_ON_FLEX);
+  form.classList.remove(SHOWING_ON_FLEX);
   greeting.classList.add(SHOWING_ON);
-  greeting.innerText = `Hello ${text}`;
+  clock.classList.add(SHOWING_ON);
+  greeting.innerText = `Hello, ${text}.`;
 }
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
