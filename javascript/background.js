4e3e9f55ea2fc08bd089a92a30081f206c2c17f5
wookkl <wjddnr315@gmail.com>
2020-10-01 17:22:13 +0900

    updated random img
    unsplash api 사용하여 랜덤 이미지를 나타내도록 함

--- javascript/background.js
+++ javascript/background.js
@@ -2,17 +2,11 @@ const body = document.querySelector("body");
 const IMG_NUMBER = 5;
 function paintImage(imgNumber) {
   const image = new Image();
-  image.src = `images/${imgNumber}.jpg`;
+  image.src = `https://source.unsplash.com/random/1600x900`;
   image.classList.add("bgImage");
   body.appendChild(image);
 }
-function genRandom() {
-  const number = Math.floor(Math.random() * IMG_NUMBER + 1);
-  return number;
-}
 function init() {
-  const randomNumber = genRandom();
-  paintImage(randomNumber);
+  paintImage();
 }
-
 init();
