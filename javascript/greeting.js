127fce7318ad55719da62e7f884eaeab25ac6475
wookkl <wjddnr315@gmail.com>
2020-10-18 23:44:49 +0900

    pending finished

--- javascript/greeting.js
+++ javascript/greeting.js
@@ -9,15 +9,18 @@ const finishedToDoList = toDoLists[1];
 const USER_LS = "currentUser",
   SHOWING_ON = "showing",
   SHOWING_ON_FLEX = "showing-flex";
+
 function saveName(text) {
   localStorage.setItem(USER_LS, text);
 }
+
 function handleSubmit(event) {
   event.preventDefault();
   const currentValue = input.value;
   paintGreeting(currentValue);
   saveName(currentValue);
 }
+
 function askForName() {
   form.classList.add(SHOWING_ON_FLEX);
   form.addEventListener("submit", handleSubmit);
@@ -32,6 +35,7 @@ function paintGreeting(text) {
   clock.classList.add(SHOWING_ON);
   greeting.innerText = `Hello, ${text}.`;
 }
+
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
 
@@ -41,7 +45,9 @@ function loadName() {
     paintGreeting(currentUser);
   }
 }
+
 function init() {
   loadName();
 }
+
 init();
--- javascript/todo.js
+++ javascript/todo.js
@@ -1,62 +1,71 @@
 const toDoForm = document.querySelector(".js-toDoForm"),
   toDoInput = toDoForm.querySelector("input"),
-  toDoList = document.querySelector(".js-toDoList");
-const TODOS_LS = "toDos";
+  lists = document.querySelectorAll(".js-toDoList");
+const pendingList = lists[0];
+const finishedList = lists[1];
+const PENDING_LS = "pending",
+  FINISHED_LS = "finished";
 
-let toDos = [];
+let pendingToDos = [],
+  finishedToDos = [];
 
-function saveToDos() {
-  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+function saveToDos(status, toDos) {
+  localStorage.setItem(status, JSON.stringify(toDos));
 }
+
 function deleteToDo(event) {
   const btn = event.target;
   const li = btn.parentNode;
-  toDoList.removeChild(li);
-  const cleanToDos = toDos.filter(function (toDo) {
+  pendingList.removeChild(li);
+  const cleanToDos = pendingToDos.filter(function (toDo) {
     return toDo.id !== parseInt(li.id);
   });
-  toDos = cleanToDos;
+  pendingToDos = cleanToDos;
   saveToDos();
 }
-function paintTodo(text) {
+
+function paintPendingTodo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
   const span = document.createElement("span");
-  const newId = toDos.length + 1;
+  const newId = pendingToDos.length + 1;
   delBtn.innerHTML = "X";
   delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
   li.appendChild(span);
   li.appendChild(delBtn);
   li.id = newId;
-  toDoList.append(li);
+  pendingList.append(li);
 
   const toDoObj = {
     text: text,
     id: newId,
   };
 
-  toDos.push(toDoObj);
-  saveToDos();
+  pendingToDos.push(toDoObj);
+  saveToDos(PENDING_LS, pendingToDos);
 }
-function loadToDos() {
-  const loadedToDos = localStorage.getItem(TODOS_LS);
+
+function loadPendingToDos() {
+  const loadedToDos = localStorage.getItem(PENDING_LS);
   if (loadedToDos !== null) {
     const parsedToDos = JSON.parse(loadedToDos);
     parsedToDos.forEach(function (toDo) {
-      paintTodo(toDo.text);
+      paintPendingTodo(toDo.text);
       toDoForm.classList.remove();
     });
   }
 }
+
 function handleSubmit(event) {
   event.preventDefault();
   const currentValue = toDoInput.value;
   paintTodo(currentValue);
   toDoInput.value = "";
 }
+
 function init() {
-  loadToDos();
+  loadPendingToDos();
   toDoForm.addEventListener("submit", handleSubmit);
 }
 
