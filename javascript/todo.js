c3ef90aa00f678c162733058a1160519eeeeb775
wookkl <wjddnr315@gmail.com>
2020-10-19 00:44:11 +0900

    store and load  pending/finished

--- javascript/todo.js
+++ javascript/todo.js
@@ -13,26 +13,89 @@ function saveToDos(status, toDos) {
   localStorage.setItem(status, JSON.stringify(toDos));
 }
 
-function deleteToDo(event) {
-  const btn = event.target;
+function deleteFinishedTodo(todo) {
+  const btn = todo;
+  const li = btn.parentNode;
+  finishedList.removeChild(li);
+  const cleanFinishedTodos = finishedToDos.filter(function (todo) {
+    return todo.id !== parseInt(li.id, 10);
+  });
+  finishedToDos = cleanFinishedTodos;
+  saveToDos(FINISHED_LS, finishedToDos);
+
+}
+
+function rePendingTodo(event) {
+  deleteFinishedTodo(event.target);
+  paintPendingTodo(event.target.parentNode.firstChild.innerText);
+}
+
+function handleDeleteFinishedToDo(event) {
+  deleteFinishedTodo(event.target);
+}
+
+function paintFinishedToDo(text) {
+  const li = document.createElement("li");
+  const rePendingBtn = document.createElement("button");
+  const delBtn = document.createElement("button");
+  const span = document.createElement("span");
+  const newId = finishedToDos.length + 1;
+  rePendingBtn.innerHTML = "🔙";
+  rePendingBtn.addEventListener("click", rePendingTodo);
+  delBtn.innerHTML = "X";
+  delBtn.addEventListener("click", handleDeleteFinishedToDo);
+
+  span.innerText = text;
+  li.appendChild(span);
+  li.appendChild(rePendingBtn);
+  li.appendChild(delBtn);
+  li.id = newId;
+  finishedList.append(li);
+
+  const toDoObj = {
+    text: text,
+    id: newId,
+  };
+
+  finishedToDos.push(toDoObj);
+  saveToDos(FINISHED_LS, finishedToDos);
+}
+
+function finishedTodo(event) {
+  deletePendingToDo(event.target);
+  paintFinishedToDo(event.target.parentNode.firstChild.innerText);
+}
+
+
+function deletePendingToDo(todo) {
+  const btn = todo;
   const li = btn.parentNode;
   pendingList.removeChild(li);
   const cleanToDos = pendingToDos.filter(function (toDo) {
     return toDo.id !== parseInt(li.id);
   });
   pendingToDos = cleanToDos;
-  saveToDos();
+  saveToDos(PENDING_LS, pendingToDos);
+}
+
+function handleDeletePendingToDo(event) {
+  deletePendingToDo(event.target);
 }
 
 function paintPendingTodo(text) {
   const li = document.createElement("li");
+  const finishedBtn = document.createElement("button");
   const delBtn = document.createElement("button");
   const span = document.createElement("span");
   const newId = pendingToDos.length + 1;
+  finishedBtn.innerHTML = "V";
+  finishedBtn.addEventListener("click", finishedTodo);
   delBtn.innerHTML = "X";
-  delBtn.addEventListener("click", deleteToDo);
+  delBtn.addEventListener("click", handleDeletePendingToDo);
+
   span.innerText = text;
   li.appendChild(span);
+  li.appendChild(finishedBtn);
   li.appendChild(delBtn);
   li.id = newId;
   pendingList.append(li);
@@ -46,13 +109,19 @@ function paintPendingTodo(text) {
   saveToDos(PENDING_LS, pendingToDos);
 }
 
-function loadPendingToDos() {
-  const loadedToDos = localStorage.getItem(PENDING_LS);
+function loadToDos(status) {
+  const loadedToDos = localStorage.getItem(status);
   if (loadedToDos !== null) {
     const parsedToDos = JSON.parse(loadedToDos);
     parsedToDos.forEach(function (toDo) {
-      paintPendingTodo(toDo.text);
-      toDoForm.classList.remove();
+      if (status === PENDING_LS) {
+        paintPendingTodo(toDo.text);
+        toDoForm.classList.remove();
+      }
+      else {
+        paintFinishedToDo(toDo.text);
+        toDoForm.classList.remove();
+      }
     });
   }
 }
@@ -60,12 +129,14 @@ function loadPendingToDos() {
 function handleSubmit(event) {
   event.preventDefault();
   const currentValue = toDoInput.value;
-  paintTodo(currentValue);
+  paintPendingTodo(currentValue);
   toDoInput.value = "";
 }
 
 function init() {
-  loadPendingToDos();
+  loadToDos(PENDING_LS);
+  loadToDos(FINISHED_LS);
+
   toDoForm.addEventListener("submit", handleSubmit);
 }
 
